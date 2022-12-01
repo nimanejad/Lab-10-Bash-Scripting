e7b98d1de080cd51eb6271d2d487b7e876920d1b
leungkingston <107340458+leungkingston@users.noreply.github.com>
2022-11-30 22:38:11 -0800

    Update question12_solutuon.bash

--- question12_solutuon.bash
+++ question12_solutuon.bash
@@ -1,1 +1,22 @@
 #!/bin/bash
+input=5
+while [ $input -ne 0 ]
+do
+    echo "Project Info"
+    echo "0 Exit"
+    echo "1 Introduction"
+    echo "2 display files"
+    echo "3 credits"
+    read input
+    if [ $input -eq 1 ]; then
+        echo An application that helps the user to find new hobbies based on their responses to certain questions.
+    elif [ $input -eq 2 ]; then
+        cd ..
+        cd Hobbyfinder-main
+        ls
+    elif [ $input -eq 3 ]; then
+        echo kingston leung, jesus arroyo, nima nejad
+        echo https://github.com/nimanejad/hobbyfinder
+	      echo CPSC 254
+    fi
+done
