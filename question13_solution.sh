f7fc45d7a75ed117b78ac052c1b9201c19d35715
leungkingston <107340458+leungkingston@users.noreply.github.com>
2022-11-30 22:53:51 -0800

    Update question13_solution.sh

--- question13_solution.sh
+++ question13_solution.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+error()
+{
+    echo "ERROR: $1" exit 1
+}
+get_md5sum()
+{
+    cat $1 | md5sum | head -c 32
+}
+name=$1 hash=$2
+if [ ! -f $name ]; then
+    error()
+    echo "File $name does not exist"
+elif [ "$(get_md5sum $name)" = "$hash" ]; then
+    echo "Success: $name matches!"
+else
+    error()
+    echo "$name does not match" 
+fi
