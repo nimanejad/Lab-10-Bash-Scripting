a1928e5aaf1052aca8e24e9617a4a1559be46d41
leungkingston <107340458+leungkingston@users.noreply.github.com>
2022-11-30 22:39:17 -0800

    Update question11_solution.sh

--- question11_solution.sh
+++ question11_solution.sh
@@ -5,23 +5,23 @@ page=_app.js
 direc3=hobbyfinder
 direc4=styles
 
-if [ -d $direc ] then
+if [ -d $direc ]; then
     cd pages
     ls
     cd ..
 fi
 
-if [ -d $direc2 ] then
+if [ -d $direc2 ]; then
     cd $direc2
-    if [ -f $page ] then
+    if [ -f $page ]; then
         echo $page
     fi
     cd ..
 fi
 
-if [ -d $direc3 ] then
+if [ -d $direc3 ]; then
     cd $direc3
-    if [ -d $direc4 ] then
+    if [ -d $direc4 ]; then
         cd $direc4
         ls
         cd ..
