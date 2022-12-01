143198eeeddb87d4ca479c58de80d222380a5291
nimanejad <47011271+nimanejad@users.noreply.github.com>
2022-11-30 22:18:11 -0800

    Update and rename question11_solution.sh to question11_solution.bash

---
+++ question11_solution.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+direc=pages
+direc2=public
+page=_app.js
+direc3=hobbyfinder
+direc4=styles
+
+if [ -d $direc ] then
+    cd pages
+    ls
+    cd ..
+fi
+
+if [ -d $direc2 ] then
+    cd $direc2
+    if [ -f $page ] then
+        echo $page
+    fi
+    cd ..
+fi
+
+if [ -d $direc3 ] then
+    cd $direc3
+    if [ -d $direc4 ] then
+        cd $direc4
+        ls
+        cd ..
+    fi
+    cd ..
+fi
